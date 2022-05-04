05ac12020acd452f2b76e9cd31666358c4b6d75a
Pabletea <pabloalonsogarc@gmail.com>
2022-05-04 10:47:42 +0200

    Main test 2

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,5 @@
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class MainTest {
 
@@ -20,10 +20,59 @@ public class MainTest {
 
         Assertions.assertEquals(ganador,pokemon2);
 
+        System.out.println("Test ganador realizado");
+
     }
 
     @Test
 
-    public
+    public void pokemonMenosPeso(){
+
+        Pokemon pokemon1= ObtenerPokemonRequest.Companion.get(5);
+
+        Pokemon pokemon2= ObtenerPokemonRequest.Companion.get(4);
+
+        Pokemon ganador = MainKt.pelea(pokemon1,pokemon2);
+
+        Assertions.assertEquals(ganador,pokemon1);
+
+        System.out.println("Test ganador 2 realizado");
+
+
+
+
+    }
+
+    @Test
+
+    public void Empate(){
+
+        Pokemon pokemon1 = ObtenerPokemonRequest.Companion.get(10);
+        Pokemon pokemon2 = ObtenerPokemonRequest.Companion.get(10);
+
+        Pokemon ganador = MainKt.pelea(pokemon1,pokemon2);
+
+        Assertions.assertNull(ganador);
+
+        System.out.println("Test empate realizado");
+
+
+
+
+    }
+
+    @Test
+    public void PokemonInvalido(){
+
+        Pokemon pokemon1 = ObtenerPokemonRequest.Companion.get(-1);
+        Pokemon pokemon2 = ObtenerPokemonRequest.Companion.get(100);
+
+
+
+        Assertions.assertThrows(PokemonInvalido.class, () -> MainKt.pelea(pokemon1,pokemon2));
+
+        System.out.println("Test inválido completado");
+
+    }
 
 }
