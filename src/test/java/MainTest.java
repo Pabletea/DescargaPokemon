b5919b40091bf79f662e5acd16ed862d4ed1032d
Pabletea <pabloalonsogarc@gmail.com>
2022-05-04 10:26:31 +0200

    Main test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,6 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class MainTest {
 
     // TODO realiza los test que consideres necesarios para comprobar que la función "pelea()" a la que le entran 2
@@ -5,6 +8,22 @@ public class MainTest {
     //  el pokémon más gordo ganará, es decir, la función devuelve al pokémon más gordo. En caso de empate (ambos pesan
     //  lo mismo) entonces la función devuelve null. En caso de que alguno de los pokémon sea null, entonces devolverá
     //  una "exception" de la clase "PokemonInvalido"
+    @Test
+    public void pokemonMasPeso(){
 
+        Pokemon pokemon1 = ObtenerPokemonRequest.Companion.get(4);
+
+        Pokemon pokemon2 = ObtenerPokemonRequest.Companion.get(5);
+
+
+        Pokemon ganador = MainKt.pelea(pokemon1,pokemon2);
+
+        Assertions.assertEquals(ganador,pokemon2);
+
+    }
+
+    @Test
+
+    public
 
 }
