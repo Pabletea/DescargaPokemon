060ef69bff25e9a63cae246c99ce59214c842a0b
Pabletea <pabloalonsogarc@gmail.com>
2022-05-10 10:00:44 +0200

    Ejercicio 4 (hecho)

--- src/main/java/Ejercicio4.java
+++ src/main/java/Ejercicio4.java
@@ -7,8 +7,33 @@ public class Ejercicio4 {
         for (Pokemon pokemon : arrayPokemons) {
             System.out.println(pokemon);
         }
+        Pokemon[] result = pokemonMasGordosQue(250);
+        for(Pokemon pokemon : result){
+            System.out.println(pokemon);
+        }
     }
 
+    public Pokemon[] pokemonMasGordosQue(Integer peso){
+        Pokemon[] out = new Pokemon[arrayPokemons.length];
+        int i=0;
+        for (Pokemon pokemon : arrayPokemons){
+            if(pokemon.getWeight() > peso){
+                out[i] = pokemon;
+                i++;
+            }
+        }
+        Pokemon[] out2 = new Pokemon[i];
 
+        i=0;
+        for(Pokemon pokemon:out){
+            if(pokemon != null){
+                out2[i]=pokemon;
+                i++;
+
+            }
+        }
+        System.out.println("Pokemons con mas peso:");
+        return out2;
+    }
 
     }
