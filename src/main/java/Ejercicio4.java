c35228512968fbb0cadd4e7b6d08bbf34e76af62
Pabletea <pabloalonsogarc@gmail.com>
2022-05-10 11:35:30 +0200

    Ejercicio 4 (hecho) 2

--- src/main/java/Ejercicio4.java
+++ src/main/java/Ejercicio4.java
@@ -8,32 +8,60 @@ public class Ejercicio4 {
             System.out.println(pokemon);
         }
         Pokemon[] result = pokemonMasGordosQue(250);
-        for(Pokemon pokemon : result){
+        Pokemon[] result2 = getPokemonMenosGordo(250);
+        System.out.println("Pokemons con mas peso:");
+        for (Pokemon pokemon : result) {
+            System.out.println(pokemon);
+        }
+        System.out.println("Pokemons con menos peso:");
+        for (Pokemon pokemon : result2) {
             System.out.println(pokemon);
         }
     }
 
-    public Pokemon[] pokemonMasGordosQue(Integer peso){
+    public Pokemon[] pokemonMasGordosQue(Integer peso) {
         Pokemon[] out = new Pokemon[arrayPokemons.length];
-        int i=0;
-        for (Pokemon pokemon : arrayPokemons){
-            if(pokemon.getWeight() > peso){
+        int i = 0;
+        for (Pokemon pokemon : arrayPokemons) {
+            if (pokemon.getWeight() > peso) {
                 out[i] = pokemon;
                 i++;
             }
         }
         Pokemon[] out2 = new Pokemon[i];
 
-        i=0;
-        for(Pokemon pokemon:out){
-            if(pokemon != null){
-                out2[i]=pokemon;
+        i = 0;
+        for (Pokemon pokemon : out) {
+            if (pokemon != null) {
+                out2[i] = pokemon;
                 i++;
 
             }
         }
-        System.out.println("Pokemons con mas peso:");
+
         return out2;
     }
 
+    public Pokemon[] getPokemonMenosGordo(Integer peso) {
+        Pokemon[] out = new Pokemon[arrayPokemons.length];
+        int i = 0;
+        for (Pokemon pokemon : arrayPokemons) {
+            if (pokemon.getWeight() < peso) {
+                out[i] = pokemon;
+                i++;
+            }
+        }
+        Pokemon[] out2 = new Pokemon[i];
+
+        i = 0;
+        for (Pokemon pokemon : out) {
+            if (pokemon != null) {
+                out2[i] = pokemon;
+                i++;
+
+            }
+        }
+
+        return out2;
     }
+}
